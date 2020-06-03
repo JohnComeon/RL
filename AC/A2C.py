ee84d23651b650760e1619ec4b27f7fc6ac2c15e
Jhon <34300982+JohnComeon@users.noreply.github.com>
2020-06-03 16:51:25 +0800

    Add files via upload

---
+++ AC/A2C.py
@@ -0,0 +1,152 @@
+# A2C 算法实现
+# 2020.5.25
+
+
+import math
+import random
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+import matplotlib.pyplot as plt
+
+
+is_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if is_cuda else "cpu")
+
+from common.multiprocessing_env import SubprocVecEnv
+
+num_envs = 8
+env_name = "CartPole-v0"
+
+def make_env():
+    def _thunk():
+        env = gym.make(env_name)
+        return env
+    return _thunk
+
+#plt.ion()
+envs = [make_env() for i in range(num_envs)]
+envs = SubprocVecEnv(envs)          # 创建8个独立的并行 envs环境
+
+env = gym.make(env_name)    # single env
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        super(ActorCritic, self).__init__()
+
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+            nn.Softmax(dim=1)       # 横向softmax
+        )
+
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+
+    def forward(self, x):
+        value = self.critic(x)
+        probs = self.actor(x)
+        dist = Categorical(probs)
+        return dist, value
+
+def test_env(vis=False):
+    state = env.reset()
+    if vis: env.render()
+    done = False
+    total_reward = 0
+    while not done:
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        dist,_ = model(state)
+        next_state, reward, done,_ = env.step(dist.sample().cpu().numpy()[0])
+        state = next_state
+        if vis: env.render()
+        total_reward += reward
+    return total_reward
+
+def compute_returns(next_value, rewards, done_mask, gamma=0.99):
+    """计算Returns"""
+    R = next_value
+    returns = []
+    for step in reversed(range(len(rewards))):
+        R = rewards[step] + gamma * R * done_mask[step]
+        returns.insert(0,R)
+    return returns
+
+def plot(frame_idx, rewards):
+    plt.plot(rewards,'b-')
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.pause(0.0001)
+
+num_inputs = envs.observation_space.shape[0]
+num_outputs = envs.action_space.n
+hidden_size = 256
+lr = 1e-3
+num_steps = 5
+
+model = ActorCritic(num_inputs,num_outputs,hidden_size).to(device)
+optimizer = optim.Adam(model.parameters())
+
+max_frames = 20000
+frame_idx = 0
+test_rewards = []
+
+state = envs.reset()
+
+
+while frame_idx < max_frames:
+    log_probs = []
+    values = []
+    rewards = []
+    done_masks = []
+    entropy = 0
+
+    for _ in range(num_steps):
+        state = torch.FloatTensor(state).to(device)
+        dist, value = model(state)
+
+        action = dist.sample()
+        next_state, reward, done, _ = envs.step(action.cpu().numpy())
+
+        log_prob = dist.log_prob(action)
+        entropy += dist.entropy().mean()
+
+        log_probs.append(log_prob)
+        values.append(value)
+        rewards.append(torch.FloatTensor(reward).unsqueeze(1).to(device))
+        done_masks.append(torch.FloatTensor(1-done).unsqueeze(1).to(device))
+
+        state = next_state
+        frame_idx += 1
+
+        if frame_idx % 100 == 0:
+            test_rewards.append(np.mean([test_env() for _ in range(10)]))
+            plot(frame_idx,test_rewards)
+
+        next_state = torch.FloatTensor(next_state).to(device)
+        _, next_value = model(next_state)
+        returns = compute_returns(next_value, rewards, done_masks)
+
+        log_probs = torch.cat(log_probs)
+        returns = torch.cat(returns).detach()     #
+        values = torch.cat(values)
+
+        advantage = returns - values
+        actor_loss = -(log_probs * advantage.detach()).mean()
+        critic_loss = advantage.pow(2).mean()       # MSE loss
+        loss = actor_loss + 0.5*critic_loss - 0.001*entropy     # entropy loss 鼓励熵增
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+#test_env(True)
